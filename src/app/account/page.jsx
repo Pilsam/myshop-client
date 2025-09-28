2cd138d13f9a7f85a24fc9db3b5ec0d877abdd93
Pilsam Hezron <54393266+Pilsam@users.noreply.github.com>
2025-09-28 23:37:53 +0300

    sad

--- src/app/account/page.jsx
+++ src/app/account/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../../firebase";
 
 const OrdersPage = () => {
   const [orders, setOrders] = useState([]);
